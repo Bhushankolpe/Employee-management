e1c5fc2fd2979bd2f2b7a22d63425dcad20dcb11
Bhushankolpe <92618968+Bhushankolpe@users.noreply.github.com>
2022-09-01 09:41:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbaar from './components/Navbaar';
+import Home from './components/Home';
+
+function App() {
+  return (
+     <>
+     <Navbaar/>
+     <Home/>
+     </>
+  );
+}
+
+export default App;
